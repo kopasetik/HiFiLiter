9b3fc76c0a599f4cc851087fe53026dee0e8b0e7
kopasetik <wale@screenverve.com>
2014-02-09 22:24:54 -0500

    separated html, css, js

---
+++ hifilighter_front_end.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+        //Toggles "prompt" class nodes/elements
+        $("body").click(function() {
+            $(".prompt").toggle();
+        });
+
+        //TODO make a function that properly can...
+        // I) return *.toString() of selections
+        var selectedText = document.getSelection().toString();
+
+        // II) add tags (w/ or w/o custom classes/attributes) to those selections)
+            // The essentials of this part are the following:
+            // T
+
+
+    });
