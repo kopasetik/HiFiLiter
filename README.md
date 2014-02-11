6c8b96e6a431db620d6c1917775f0ee4b9ddbb20
kopasetik <wale@screenverve.com>
2014-02-10 21:57:19 -0500

    started readme

---
+++ README.md
@@ -0,0 +1,4 @@
+*WORK IN PROGRESS*
+This will be a plugin for highlighting text on your blog and news site. The default will be that it highlights text yellow and copies the text to a social media sharing feature.
+
+As foreseen, it will be required that you use special tags/attributes for your article paragraphs and possibly incorporate some other light html/css/javascript.
--- hifilighter_front_end.js
+++ hifilighter_front_end.js
@@ -1,51 +1,67 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-    //Toggles "prompt" class nodes/elements
-    $("body").click(function() {
-        $(".prompt").toggle();
-    });
+//     //Toggles "prompt" class nodes/elements
+//     $("body").click(function() {
+//         $(".prompt").toggle();
+//     });
 
 
-    //This happens when the person finishes highlighting
-    $("p").mouseup(function(){
-        
-        //TODO make a function that properly can...
-        // I) return *.toString() of selections
-        // Looks like I need to put this in an event (to create a closure?)
-        // so that it shows up in the console
-        var selObj = document.getSelection();
-        var selRange = selObj.getRangeAt();
-        // alert(selectedText);
+//     //This happens when the person finishes highlighting
+//     $("p").mouseup(function(){
 
-        // II) add tags (w/ or w/o custom classes/attributes) to those selections
-            // The essentials of this part are the following:
-            // 1) See whether the selected text is in a single node
-            var singleNode = ( selObj.focusNode == selObj.anchorNode )
-            // 2) If yes, jump to step 5. If no, proceed to step 3
-            ranges = []
-            if ( singleNode ) {
-                ranges.push(selObj.getRangeAt().surroundContents())
-            } else {
-            // 3) Count the number of nodes across which the selected text spans
+//         //TODO make a function that properly can...
+//         // I) return *.toString() of selections
+//         // Looks like I need to put this in an event (to create a closure?)
+//         // so that it shows up in the console
+//         var selObj = document.getSelection();
+//         var selRange = selObj.getRangeAt();
+//         var singleNode = function(){
+            // return ( selObj.focusNode == selObj.anchorNode );
+        // };
+//         var ranges = [];
+//         var newNode = $('<strong style="background-color: yellow"></strong>');
+//         // alert(selectedText);
+
+//         // II) add tags (w/ or w/o custom classes/attributes) to those selections
+//             // The essentials of this part are the following:
+//             // 1) See whether the selected text is in a single node
+//             if ( singleNode ) {
             
-            // 4) Determine the ranges of the selected text for each node
+//             // 2) If yes, jump to step 5. If no, proceed to step 3
             
-            }
+//                 // ranges.push(selObj.getRangeAt());
+//                //ranges.forEach(function(element, idx, arr){
+//                      element.surroundContents()
+//                  });
+//             } else {
+//             // 3) Count the number of nodes across which the selected text spans
             
-            // 5) Surround the range(s) with tags
+//             // 4) Determine the ranges of the selected text for each node
             
-            // 6) Toggle '.hilited' class for the tags
+//             }
             
-        // III) Put selection text into a margin popover
-            // 1) Get rid of any extra whitespace and/or carriage returns
+//             // 5) Surround the range(s) with tags
             
-            // 2) Determine whether popover should be on left or right side
+//             // 6) Toggle '.hilited' class for the tags
             
-            // 3) Create a popover
+//         // III) Put selection text into a margin popover
+//             // 1) Get rid of any extra whitespace and/or carriage returns
             
-            // 4) Populate popover with selection text
+//             // 2) Determine whether popover should be on left or right side
             
-            // 5) Include Twitter and Facebook sharing buttons
+//             // 3) Create a popover
             
-    });
-});
+//             // 4) Populate popover with selection text
+            
+//             // 5) Include Twitter and Facebook sharing buttons
+            
+//     });
+// });
+
+var selObj = document.getSelection();
+        // var selRange = selObj.getRangeAt();
+        var singleNode = function(){
+            return ( selObj.focusNode == selObj.anchorNode );
+        };
+        var ranges = [];
+        var newNode = $('<strong style="background-color: yellow"></strong>');
