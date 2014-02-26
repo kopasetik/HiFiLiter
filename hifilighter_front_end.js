cf6ff2e60b21b52f07d61b70bbf181270c11692b
kopasetik <wale@screenverve.com>
2014-02-25 23:52:04 -0500

    shuffle comments

--- hifilighter_front_end.js
+++ hifilighter_front_end.js
@@ -15,8 +15,8 @@
 //         // so that it shows up in the console
 //         var selObj = document.getSelection();
 //         var singleNode = function(){
-            // return ( selObj.focusNode == selObj.anchorNode );
-        // };
+//             return ( selObj.focusNode == selObj.anchorNode );
+//         };
 //         var ranges = [];
 //         var newNode = $('<strong style="background-color: yellow"></strong>');
 //         // alert(selectedText);
@@ -24,34 +24,34 @@
 //         // II) add tags (w/ or w/o custom classes/attributes) to those selections
 //             // The essentials of this part are the following:
 //             // 1) See whether the selected text is in a single node
-                // var singleNode = function(){
-                //     return ( selObj.focusNode == selObj.anchorNode );
-                // };
+//                 var singleNode = function(){
+//                     return ( selObj.focusNode == selObj.anchorNode );
+//                 };
 //             if ( singleNode ) {
             
 //             // 2) If yes, jump to step 5. If no, proceed to step 3
             
-//                 // ranges.push(selObj.getRangeAt());
-//                //ranges.forEach(function(element, idx, arr){
-//                      element.surroundContents()
-//                  });
+//                     // ranges.push(selObj.getRangeAt());
+//                     // ranges.forEach(function(element, idx, arr){
+//                     //     element.surroundContents()
+//                     // });
 //             } else {
 //             // 3) Count the number of nodes across which the selected text spans
-            // $('p').index(selObj.focusNode.parentElement) - $('p').index(selObj.anchorNode.parentElement)
+//             $('p').index(selObj.focusNode.parentElement) - $('p').index(selObj.anchorNode.parentElement)
 //             // 4) Determine the ranges of the selected text for each node
             
 //             }
             
 //             // 5) Surround the range(s) with tags & 6) Toggle '.hilited' class for the tags
-                // selObj.getRangeAt().surroundContents($(document.createElement("span")).toggleClass('hilited')[0]);
+//                 selObj.getRangeAt().surroundContents($(document.createElement("span")).toggleClass('hilited')[0]);
 
 //         // III) Put selection text into a margin popover
 //             // 1) Get rid of any extra whitespace and/or carriage returns
             
 //             // 2) Create a popover
-                // $(selObj.anchorNode.parentElementanchorObj).attr("data-toggle", "popover");
-                // $(selObj.anchorNode.parentElementanchorObj).attr("data-content", "Hi again!");
-                // $(selObj.anchorNode.parentElementanchorObj).popover('show');
+//                 $(selObj.anchorNode.parentElementanchorObj).attr("data-toggle", "popover");
+//                 $(selObj.anchorNode.parentElementanchorObj).attr("data-content", "Hi again!");
+//                 $(selObj.anchorNode.parentElementanchorObj).popover('show');
             
 //             // 3) Populate popover with selection text
             
@@ -70,6 +70,7 @@ $(document).ready(function() {
         selObj.getRangeAt().surroundContents($(document.createElement("span")).toggleClass('hilited')[0]);
         // TODO - push range location data for each selection to an array/object/json-file
         // And put in an animation for the highlighting
+        
     });
 
 });
