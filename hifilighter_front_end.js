5aef42e830403deb5f9f42f04a0acf49d1cc18b1
kopasetik <wale@screenverve.com>
2014-03-21 21:43:28 -0400

    attempt to make it fcnal

--- hifilighter_front_end.js
+++ hifilighter_front_end.js
@@ -1,106 +1,104 @@
 $(document).ready(function() {
-
-        var
-            selObj = document.getSelection(),
-            ranges = [];
-        var
-            nodesArray = Array.prototype.slice.call(document.querySelectorAll("p")),
-            singleNode = function(){
-            return ( selObj.focusNode == selObj.anchorNode );
-        };
-            var nodeCount = nodesArray.indexOf(selObj.focusNode.parentElement) - nodesArray.indexOf(selObj.anchorNode.parentElement);
+    var ranges = [];
     
-                var 
-                    newRangeFirst = selObj.getRangeAt().cloneRange(),
-                    newRangeLast = newRangeFirst.cloneRange();
-                    newRng = document.createRange();
-
-    function surroundIt() {
-        ranges.forEach(function( element, idx, arr ){
-            element.surroundContents(makeHighligtedSpan());
-        });
+    function nodesArray() {
+        return Array.prototype.slice.call(document.querySelectorAll("p"));
+    }
         
+    function newRangeFirst() {
+        return selObj().getRangeAt().cloneRange();
     }
 
-    var liteItUp = function() {
-        //The function should:
-        // I) create a selection variable and an array for ranges
+    function newRangeLast() {
+        return newRangeFirst().cloneRange();
+    }
 
-        // II) add tags (w/ or w/o custom classes/attributes) to those selections
+    function newRng() {
+        return document.createRange();
+    }
+
+    function nodeCount() {
+        return nodesArray().indexOf(selObj().focusNode.parentElement) - nodesArray().indexOf(selObj().anchorNode.parentElement);
+    }
+
+    function selObj() {
+        return document.getSelection();
+    }
+
+
+    function singleNode() {
+        return ( selObj().focusNode == selObj().anchorNode );
+    }
+
+
+    function adjustEdgeRanges() {
+        newRangeFirst().setEnd( newRangeFirst().startContainer, newRangeFirst().startContainer.length );
+        newRangeLast().setStart( newRangeLast().endContainer, 0 );
+    }
+
+    function adjustPushMiddleRanges() {    
+        for ( var i = nodeCount()-1; i > 0; i-- ) {
+            newRng().selectNodeContents(nodesArray()[nodesArray().indexOf(selObj().anchorNode.parentElement) + i]);
+            ranges.push(newRng().cloneRange());
+        }
+    }
+
+    function pushEdgeRanges() {
+        if (singleNode) {
+            ranges.push(selObj().getRangeAt());
+            return;
+        }
+
+        ranges.push( newRangeFirst() );
+        ranges.push( newRangeLast() );
+        return;
+    }
+
+    function makeHighligtedSpan(){
+        var el = document.createElement("span");
+        el.classList.toggle("hilited");
+        return el;
+    }
+
+    function surroundIt(){
+        ranges.forEach(function( element, idx, arr ){
+            element.surroundContents(makeHighligtedSpan());
+        });        
+    }
+
+    function liteItUp() {
+
+        //The function should:
+        // I) add tags (w/ or w/o custom classes/attributes) to those selections
+
         // The essentials of this part are the following:
         // 1) See whether the selected text is in a single node
 
         if ( singleNode() ) {
-            
-        // 2) If yes, jump to step 5. If no, proceed to step 3
-            ranges.push(selObj.getRangeAt());
+        // 2) If yes, jump to step 4. If no, proceed to step 3
+            pushEdgeRanges();
         } else {
-        // 3) Count the number of nodes across which the selected text spans
 
-        // 4) Determine the ranges of the selected text for each node
-            if (nodeCount == 1) {
-                newRangeFirst.setEnd( newRangeFirst.startContainer, newRangeFirst.startContainer.length );
-                newRangeLast.setStart( newRangeLast.endContainer, 0 );
-                ranges.push( newRangeFirst );
-                ranges.push( newRangeLast );
+        // 3) Determine the ranges of the selected text for each node
+            if (nodeCount() == 1) {
+                adjustEdgeRanges();
+                pushEdgeRanges();                
             } else {
-                newRangeFirst.setEnd( newRangeFirst.startContainer, newRangeFirst.startContainer.length );
-                newRangeLast.setStart( newRangeLast.endContainer, 0 );
-                ranges.push( newRangeFirst );
-                ranges.push( newRangeLast );
-
-            // Get highlighting for the nodes in between the first and last ones
-                for ( var i = nodeCount-1; i > 0; i-- ) {
-                    newRng.selectNodeContents(nodesArray[nodesArray.indexOf(selObj.anchorNode.parentElement) + i]);
-                    ranges.push(newRng.cloneRange());
-                }
-
+                adjustEdgeRanges();
+                adjustPushMiddleRanges();
+                pushEdgeRanges();
             }
                         
         }
             
-        // 5) Surround the range(s) with tags & 6) Toggle '.hilited' class for the tags
+        // 4) Surround the range(s) with tags & toggle '.hilited' class for the tags
         surroundIt();
 
-        //What happens when a highlight is selected again? Or when it's clicked?
-        // Possibility: toggle the highlighting on mouseover
-        // $(".hilited").mouseenter(function(){
-        //     $(this).toggleClass("hilited");
-        // });
-
-        // $(".hilited").mouseleave(function(){
-        //     $(this).toggleClass("hilited");
-        // });
-
-        // 7) Collapse any remaining selection
+        // 5) Collapse any remaining selection(s)
         document.getSelection().collapse();
+    }
+        
 
-//      // III) Put selection text into a margin popover
-//      // 1) Get rid of any extra whitespace and/or carriage returns
-            
-//      // 2) Create a popover
-//          $(selObj.anchorNode.parentElementanchorObj).attr("data-toggle", "popover");
-//          $(selObj.anchorNode.parentElementanchorObj).attr("data-content", "Hi again!");
-//          $(selObj.anchorNode.parentElementanchorObj).popover('show');
-
-//      // 3) Populate popover with selection text
-
-//      // 4) Include Twitter and Facebook sharing buttons
-//         // TODO - push range location data for each selection to an array/object/json-file
-//         // And put in an animation for the highlighting/unhighlighting
-    };
-
-    var parseHTML = function(str) {
-        var tmp = document.implementation.createHTMLDocument();
-        tmp.body.innerHTML = str;
-        return tmp.body.children;
-    };
-
-    var makeHighligtedSpan = function(){
-        var el = document.createElement("span");
-        el.classList.toggle("hilited");
-        return el;
-    };
 
     // This happens when the person finishes highlighting
     $("p").mouseup(function(){
@@ -113,6 +111,33 @@ $(document).ready(function() {
         liteItUp();
 
     });
+
+});
+
+        //What happens when a highlight is selected again? Or when it's clicked?
+        // Possibility: toggle the highlighting on mouseover
+        // $(".hilited").mouseenter(function(){
+        //     $(this).toggleClass("hilited");
+        // });
+
+        // $(".hilited").mouseleave(function(){
+        //     $(this).toggleClass("hilited");
+        // });
+
+
+//      // II) Put selection text into a margin popover
+//      // 1) Get rid of any extra whitespace and/or carriage returns
+            
+//      // 2) Create a popover
+//          $(selObj().anchorNode.parentElementanchorObj).attr("data-toggle", "popover");
+//          $(selObj().anchorNode.parentElementanchorObj).attr("data-content", "Hi again!");
+//          $(selObj().anchorNode.parentElementanchorObj).popover('show');
+
+//      // 3) Populate popover with selection text
+
+//      // 4) Include Twitter and Facebook sharing buttons
+//         // TODO - push range location data for each selection to an array/object/json-file
+//         // And put in an animation for the highlighting/unhighlighting
 
     // $(".hilited").mouseenter(function(){
     //     $(this).toggleClass("hilited");
@@ -127,5 +152,3 @@ $(document).ready(function() {
         // 2) If the whole selection is a highlight already, remove the highlight
         // element.outerHTML = element.innerHTML
 //
-
-});
