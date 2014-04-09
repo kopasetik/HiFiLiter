068d76f37fc0edb9be63f6262b7909e1a58b0a6d
kopasetik <wale@screenverve.com>
2014-04-09 10:15:47 -0400

    qunit testing setup

---
+++ tests.js
@@ -0,0 +1,8 @@
+test("First test", function(){
+
+    equal( convertToArticleJSON(), {article: {
+        "title": "Hifilighter Front Page", 
+        "url": "file:///home/wale/Code/GitHub/HiFiLiter/hifilighter_front_end.html"
+    }});
+
+});
