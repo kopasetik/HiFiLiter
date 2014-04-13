706e233648f6b8b03c01b0bd665c2d0c23636e02
kopasetik <wale@screenverve.com>
2014-04-13 18:44:38 -0400

    update to tests

--- tests.js
+++ tests.js
@@ -1,19 +1,19 @@
 test("First test", function(){
 
-    equal( selObj(), document.getSelection());
+    deepEqual( selObj(), document.getSelection());
 
 });
 
 test("Tough tests", function(){
 
-    equal( convertToArticleJSON(), {
+    deepEqual( convertToArticleJSON(), {
         article: {
             "title": "Hifilighter Front Page", 
             "url": "file:///home/wale/Code/GitHub/HiFiLiter/hifilighter_front_end.html"
         }
     });
 
-    equal( convertToHiliteJSON( "hello" ), {
+    deepEqual( convertToHiliteJSON( "hello" ), {
         highlight: {
             "text": "hello",
             "url": "file:///home/wale/Code/GitHub/HiFiLiter/hifilighter_front_end.html"   
